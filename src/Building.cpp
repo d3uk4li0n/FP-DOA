d5feddd2a342f651fa7d18e0547f980c1c446262
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2024-03-19 19:42:55 +0100

    Fix: replace level for getlevel

--- src/Building.cpp
+++ src/Building.cpp
@@ -3,23 +3,23 @@
 int Farmland::produce() const
 {
     // Produces food
-    return 20 * level;
+    return 20 * getLevel();
 }
 
 int Mine::produce() const
 {
     // Produces metal
-    return 10 * level;
+    return 10 * getLevel();
 }
 
 int Quarry::produce() const
 {
     // Produces stone
-    return 15 * level;
+    return 15 * getLevel();
 }
 
 int LumberMill::produce() const
 {
     // Produces wood
-    return 12 * level;
+    return 12 * getLevel();
 }
